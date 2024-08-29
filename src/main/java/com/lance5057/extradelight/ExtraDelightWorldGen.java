19c73ce2ab70806a8054663c2a40ec04169846f7
unilock <unilock@fennet.rentals>
2024-08-29 14:29:57 -0400

    dimension structure datagen
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/com/lance5057/extradelight/ExtraDelightWorldGen.java
+++ src/main/java/com/lance5057/extradelight/ExtraDelightWorldGen.java
@@ -10,6 +10,7 @@ import net.minecraft.world.level.dimension.LevelStem;
 import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.structure.StructureSet;
 
 public class ExtraDelightWorldGen {
 	public static final ResourceKey<Level> CORNFIELD = ResourceKey.create(
@@ -59,4 +60,33 @@ public class ExtraDelightWorldGen {
 			Registries.PLACED_FEATURE,
 			ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "cinnamon")
 	);
+
+	public static final ResourceKey<StructureSet> STRUCTURE_SET = ResourceKey.create(
+			Registries.STRUCTURE_SET,
+			ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "structure_set")
+	);
+
+	public static final ResourceLocation BARN = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "barn");
+	public static final ResourceLocation CAMP1 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "camp1");
+	public static final ResourceLocation CAMP2 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "camp2");
+	public static final ResourceLocation DOLL_CIRCLE = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "doll_circle");
+	public static final ResourceLocation DOLL_CIRCLE1 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "doll_circle1");
+	public static final ResourceLocation DOLL_CIRCLE2 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "doll_circle2");
+	public static final ResourceLocation DOLL_CIRCLE3 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "doll_circle3");
+	public static final ResourceLocation DOLL_CIRCLE4 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "doll_circle4");
+	public static final ResourceLocation FOUNTAIN = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "fountain");
+	public static final ResourceLocation HAUNTEDHOUSE = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "hauntedhouse");
+	public static final ResourceLocation PUMPKIN_PATCH2 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "pumpkin_patch2");
+	public static final ResourceLocation PUMPKIN_PATCH3 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "pumpkin_patch3");
+	public static final ResourceLocation PUMPKIN_PATCH4 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "pumpkin_patch4");
+	public static final ResourceLocation PUMPKIN_PATCH5 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "pumpkin_patch5");
+	public static final ResourceLocation PUMPKIN_PILE1 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "pumpkin_pile1");
+	public static final ResourceLocation RAIL_STOP = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "rail_stop");
+	public static final ResourceLocation SACRIFICE = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "sacrifice");
+	public static final ResourceLocation SACRIFICE2 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "sacrifice2");
+	public static final ResourceLocation SIGN = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "sign");
+	public static final ResourceLocation TABLE1 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "table1");
+	public static final ResourceLocation TABLE2 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "table2");
+	public static final ResourceLocation TABLE3 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "table3");
+	public static final ResourceLocation TABLE4 = ResourceLocation.fromNamespaceAndPath(ExtraDelight.MOD_ID, "table4");
 }
--- src/main/java/com/lance5057/extradelight/data/EDRegistries.java
+++ src/main/java/com/lance5057/extradelight/data/EDRegistries.java
@@ -2,16 +2,19 @@ package com.lance5057.extradelight.data;
 
 import com.lance5057.extradelight.ExtraDelight;
 import com.lance5057.extradelight.ExtraDelightBlocks;
-import com.lance5057.extradelight.ExtraDelightWorldGen;
 import com.lance5057.extradelight.worldgen.features.ExtraDelightFeatures;
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.worldgen.Pools;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.data.worldgen.placement.VegetationPlacements;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.valueproviders.ConstantInt;
@@ -32,9 +35,18 @@ import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSi
 import net.minecraft.world.level.levelgen.feature.foliageplacers.RandomSpreadFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.BendingTrunkPlacer;
+import net.minecraft.world.level.levelgen.heightproviders.ConstantHeight;
 import net.minecraft.world.level.levelgen.placement.CountPlacement;
 import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureSet;
+import net.minecraft.world.level.levelgen.structure.TerrainAdjustment;
+import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadStructurePlacement;
+import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadType;
+import net.minecraft.world.level.levelgen.structure.pools.StructurePoolElement;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import net.minecraft.world.level.levelgen.structure.structures.JigsawStructure;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
 
 import java.util.List;
@@ -43,13 +55,15 @@ import java.util.OptionalLong;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
+import static com.lance5057.extradelight.ExtraDelightWorldGen.*;
+
 public class EDRegistries {
     private static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.CONFIGURED_FEATURE, bootstrap -> {
-                bootstrap.register(ExtraDelightWorldGen.CONFIGURED_CORN_MAZE, new ConfiguredFeature<>(ExtraDelightFeatures.CORN_MAZE_FEATURE.get(), new SimpleBlockConfiguration(BlockStateProvider.simple(ExtraDelightBlocks.CORN_BOTTOM.get()))));
-                bootstrap.register(ExtraDelightWorldGen.CONFIGURED_CORN_RAIL, new ConfiguredFeature<>(ExtraDelightFeatures.CORN_RAIL_FEATURE.get(), new SimpleBlockConfiguration(BlockStateProvider.simple(Blocks.RAIL))));
+                bootstrap.register(CONFIGURED_CORN_MAZE, new ConfiguredFeature<>(ExtraDelightFeatures.CORN_MAZE_FEATURE.get(), new SimpleBlockConfiguration(BlockStateProvider.simple(ExtraDelightBlocks.CORN_BOTTOM.get()))));
+                bootstrap.register(CONFIGURED_CORN_RAIL, new ConfiguredFeature<>(ExtraDelightFeatures.CORN_RAIL_FEATURE.get(), new SimpleBlockConfiguration(BlockStateProvider.simple(Blocks.RAIL))));
 
-                bootstrap.register(ExtraDelightWorldGen.CONFIGURED_CINNAMON_TREE, new ConfiguredFeature<>(Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
+                bootstrap.register(CONFIGURED_CINNAMON_TREE, new ConfiguredFeature<>(Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
                         BlockStateProvider.simple(ExtraDelightBlocks.CINNAMON_LOG.get()),
                         new BendingTrunkPlacer(3, 0, 3, 6, ConstantInt.of(1)),
                         BlockStateProvider.simple(ExtraDelightBlocks.CINNAMON_LEAVES.get()),
@@ -59,30 +73,30 @@ public class EDRegistries {
             })
             .add(Registries.PLACED_FEATURE, bootstrap -> {
                 HolderGetter<ConfiguredFeature<?, ?>> cfgs = bootstrap.lookup(Registries.CONFIGURED_FEATURE);
-                bootstrap.register(ExtraDelightWorldGen.PLACED_CORN_MAZE, new PlacedFeature(cfgs.getOrThrow(ExtraDelightWorldGen.CONFIGURED_CORN_MAZE), List.of(
+                bootstrap.register(PLACED_CORN_MAZE, new PlacedFeature(cfgs.getOrThrow(CONFIGURED_CORN_MAZE), List.of(
                         CountPlacement.of(1),
                         InSquarePlacement.spread(),
                         PlacementUtils.HEIGHTMAP_WORLD_SURFACE
                 )));
-                bootstrap.register(ExtraDelightWorldGen.PLACED_CORN_RAIL, new PlacedFeature(cfgs.getOrThrow(ExtraDelightWorldGen.CONFIGURED_CORN_RAIL), List.of(
+                bootstrap.register(PLACED_CORN_RAIL, new PlacedFeature(cfgs.getOrThrow(CONFIGURED_CORN_RAIL), List.of(
                         CountPlacement.of(1),
                         InSquarePlacement.spread(),
                         PlacementUtils.HEIGHTMAP_WORLD_SURFACE
                 )));
 
-                bootstrap.register(ExtraDelightWorldGen.PLACED_CINNAMON_TREE, new PlacedFeature(cfgs.getOrThrow(ExtraDelightWorldGen.CONFIGURED_CINNAMON_TREE), List.of(
+                bootstrap.register(PLACED_CINNAMON_TREE, new PlacedFeature(cfgs.getOrThrow(CONFIGURED_CINNAMON_TREE), List.of(
                         PlacementUtils.filteredByBlockSurvival(ExtraDelightBlocks.CINNAMON_SAPLING.get())
                 )));
             })
             .add(Registries.BIOME, bootstrap -> {
                 HolderGetter<PlacedFeature> placedFeatures = bootstrap.lookup(Registries.PLACED_FEATURE);
-                bootstrap.register(ExtraDelightWorldGen.CORNFIELD_BIOME, new Biome.BiomeBuilder()
+                bootstrap.register(CORNFIELD_BIOME, new Biome.BiomeBuilder()
                         .downfall(0.4F)
                         .generationSettings(new BiomeGenerationSettings(
                                 Map.of(),
                                 List.of(
                                         HolderSet.direct(
-                                                placedFeatures.getOrThrow(ExtraDelightWorldGen.PLACED_CORN_RAIL),
+                                                placedFeatures.getOrThrow(PLACED_CORN_RAIL),
                                                 placedFeatures.getOrThrow(VegetationPlacements.PATCH_PUMPKIN)
                                         ),
                                         HolderSet.empty(),
@@ -94,7 +108,7 @@ public class EDRegistries {
                                         HolderSet.empty(),
                                         HolderSet.empty(),
                                         HolderSet.direct(
-                                                placedFeatures.getOrThrow(ExtraDelightWorldGen.PLACED_CORN_MAZE)
+                                                placedFeatures.getOrThrow(PLACED_CORN_MAZE)
                                         )
                                 )
                         ))
@@ -124,7 +138,7 @@ public class EDRegistries {
                 );
             })
             .add(Registries.DIMENSION_TYPE, bootstrap -> {
-                bootstrap.register(ExtraDelightWorldGen.CORNFIELD_TYPE, new DimensionType(
+                bootstrap.register(CORNFIELD_TYPE, new DimensionType(
                         OptionalLong.of(18000),
                         true,
                         false,
@@ -151,15 +165,15 @@ public class EDRegistries {
                 HolderGetter<Biome> biomes = bootstrap.lookup(Registries.BIOME);
                 HolderGetter<DimensionType> dimensionTypes = bootstrap.lookup(Registries.DIMENSION_TYPE);
                 HolderGetter<NoiseGeneratorSettings> noiseSettings = bootstrap.lookup(Registries.NOISE_SETTINGS);
-                BiomeSource biomeSource = new FixedBiomeSource(biomes.getOrThrow(ExtraDelightWorldGen.CORNFIELD_BIOME));
-                NoiseBasedChunkGenerator chunkGenerator = new NoiseBasedChunkGenerator(biomeSource, noiseSettings.getOrThrow(ExtraDelightWorldGen.CORNFIELD_NOISE));
-                bootstrap.register(ExtraDelightWorldGen.CORNFIELD_STEM, new LevelStem(
-                        dimensionTypes.getOrThrow(ExtraDelightWorldGen.CORNFIELD_TYPE),
+                BiomeSource biomeSource = new FixedBiomeSource(biomes.getOrThrow(CORNFIELD_BIOME));
+                NoiseBasedChunkGenerator chunkGenerator = new NoiseBasedChunkGenerator(biomeSource, noiseSettings.getOrThrow(CORNFIELD_NOISE));
+                bootstrap.register(CORNFIELD_STEM, new LevelStem(
+                        dimensionTypes.getOrThrow(CORNFIELD_TYPE),
                         chunkGenerator
                 ));
             })
             .add(Registries.NOISE_SETTINGS, bootstrap -> {
-                bootstrap.register(ExtraDelightWorldGen.CORNFIELD_NOISE, new NoiseGeneratorSettings(
+                bootstrap.register(CORNFIELD_NOISE, new NoiseGeneratorSettings(
                         new NoiseSettings(
                                 0,
                                 128,
@@ -198,10 +212,79 @@ public class EDRegistries {
                         false,
                         false
                 ));
+            })
+            .add(Registries.STRUCTURE, bootstrap -> {
+                HolderGetter<Biome> biomes = bootstrap.lookup(Registries.BIOME);
+                HolderGetter<StructureTemplatePool> templatePools = bootstrap.lookup(Registries.TEMPLATE_POOL);
+                for (ResourceLocation id : List.of(BARN, CAMP1, CAMP2, DOLL_CIRCLE, DOLL_CIRCLE1, DOLL_CIRCLE2, DOLL_CIRCLE3, DOLL_CIRCLE4, FOUNTAIN, HAUNTEDHOUSE, PUMPKIN_PATCH2, PUMPKIN_PATCH3, PUMPKIN_PATCH4, PUMPKIN_PATCH5, PUMPKIN_PILE1, SACRIFICE, SACRIFICE2, SIGN, TABLE1, TABLE2, TABLE3, TABLE4)) {
+                    bootstrap.register(ResourceKey.create(Registries.STRUCTURE, id), new JigsawStructure(
+                            new Structure.StructureSettings(HolderSet.direct(biomes.getOrThrow(CORNFIELD_BIOME)), Map.of(), GenerationStep.Decoration.SURFACE_STRUCTURES, TerrainAdjustment.BEARD_THIN),
+                            templatePools.getOrThrow(ResourceKey.create(Registries.TEMPLATE_POOL, id)),
+                            1,
+                            ConstantHeight.ZERO,
+                            false,
+                            Heightmap.Types.WORLD_SURFACE_WG
+                    ));
+                }
+                bootstrap.register(ResourceKey.create(Registries.STRUCTURE, RAIL_STOP), new JigsawStructure(
+                        new Structure.StructureSettings(HolderSet.direct(biomes.getOrThrow(CORNFIELD_BIOME)), Map.of(), GenerationStep.Decoration.SURFACE_STRUCTURES, TerrainAdjustment.NONE),
+                        templatePools.getOrThrow(ResourceKey.create(Registries.TEMPLATE_POOL, RAIL_STOP)),
+                        1,
+                        ConstantHeight.ZERO,
+                        false,
+                        Heightmap.Types.WORLD_SURFACE_WG
+                ));
+            })
+            .add(Registries.STRUCTURE_SET, bootstrap -> {
+                HolderGetter<Structure> structures = bootstrap.lookup(Registries.STRUCTURE);
+                bootstrap.register(STRUCTURE_SET, new StructureSet(
+                        List.of(
+                                entry(BARN, 1, structures),
+                                entry(CAMP1, 5, structures),
+                                entry(CAMP2, 5, structures),
+                                entry(DOLL_CIRCLE, 25, structures),
+                                entry(DOLL_CIRCLE1, 25, structures),
+                                entry(DOLL_CIRCLE2, 25, structures),
+                                entry(DOLL_CIRCLE3, 25, structures),
+                                entry(DOLL_CIRCLE4, 25, structures),
+                                entry(FOUNTAIN, 5, structures),
+                                entry(HAUNTEDHOUSE, 1, structures),
+                                entry(PUMPKIN_PATCH2, 100, structures),
+//                                entry(PUMPKIN_PATCH3, 100, structures),
+                                entry(PUMPKIN_PATCH4, 100, structures),
+                                entry(PUMPKIN_PATCH5, 100, structures),
+                                entry(PUMPKIN_PILE1, 100, structures),
+                                entry(SACRIFICE, 1, structures),
+                                entry(SACRIFICE2, 1, structures),
+                                entry(SIGN, 1, structures),
+                                entry(TABLE1, 25, structures),
+                                entry(TABLE2, 25, structures),
+                                entry(TABLE3, 25, structures),
+                                entry(TABLE4, 25, structures)
+                        ),
+                        new RandomSpreadStructurePlacement(
+                                15,
+                                5,
+                                RandomSpreadType.LINEAR,
+                                1426250756
+                        )
+                ));
+            })
+            .add(Registries.TEMPLATE_POOL, bootstrap -> {
+                HolderGetter<StructureTemplatePool> templatePools = bootstrap.lookup(Registries.TEMPLATE_POOL);
+                for (ResourceLocation id : List.of(BARN, CAMP1, CAMP2, DOLL_CIRCLE, DOLL_CIRCLE1, DOLL_CIRCLE2, DOLL_CIRCLE3, DOLL_CIRCLE4, FOUNTAIN, HAUNTEDHOUSE, PUMPKIN_PATCH2, PUMPKIN_PATCH3, PUMPKIN_PATCH4, PUMPKIN_PATCH5, PUMPKIN_PILE1, RAIL_STOP, SACRIFICE, SACRIFICE2, SIGN, TABLE1, TABLE2, TABLE3, TABLE4)) {
+                    bootstrap.register(ResourceKey.create(Registries.TEMPLATE_POOL, id), new StructureTemplatePool(
+                            templatePools.getOrThrow(Pools.EMPTY),
+                            List.of(
+                                    Pair.of(
+                                            StructurePoolElement.single(id.getPath()),
+                                            1
+                                    )
+                            ),
+                            StructureTemplatePool.Projection.RIGID
+                    ));
+                }
             });
-            /*.add(Registries.STRUCTURE, bootstrap -> {
-                
-            })*/
 
     public static DatapackBuiltinEntriesProvider provider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider) {
         return new DatapackBuiltinEntriesProvider(
@@ -210,5 +293,9 @@ public class EDRegistries {
                 BUILDER,
                 Set.of(ExtraDelight.MOD_ID)
         );
+    }
+
+    private static StructureSet.StructureSelectionEntry entry(ResourceLocation id, int weight, HolderGetter<Structure> lookup) {
+        return new StructureSet.StructureSelectionEntry(lookup.getOrThrow(ResourceKey.create(Registries.STRUCTURE, id)), weight);
     }
 }
